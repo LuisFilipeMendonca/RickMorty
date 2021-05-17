7f97ae08082af537f7da0e09ef4cf5fd4ca67c39
LuisFilipeMendonca <filipedev@outlook.pt>
2021-05-17 14:01:56 +0100

    Updated carousel item style

--- src/components/CharacterItem/index.js
+++ src/components/CharacterItem/index.js
@@ -2,11 +2,11 @@ import React from "react";
 
 import "./index.css";
 
-const CharacterItem = ({ data, style }) => {
+const CharacterItem = ({ data, style, key }) => {
   const { image, name } = data;
   return (
     <div className={`character ${style}`}>
-      <img className="character__img" src={image} />
+      <img className="character__img" src={image} alt={name} />
       <p className="character__txt">{name}</p>
     </div>
   );
